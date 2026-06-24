4d448f20bb3b15bff0e81eeca255f63d1b791067
Kalpesh <kalpeshpatil5157@gmail.com>
2026-06-24 12:10:52 +0530

    Add production knex config

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,14 +1,26 @@
 require('dotenv').config();
 
-module.exports = {
-  development: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: './db/migrations'
-    },
-    seeds: {
-      directory: './db/seeds'
-    }
+const config = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    directory: './db/migrations'
+  },
+  seeds: {
+    directory: './db/seeds'
   }
 };
+
+module.exports = {
+  development: config,
+
+  production: {
+    ...config,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  }
+}
